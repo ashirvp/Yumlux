35c04b28855b9659890bcaf2eb7e6149a846a34e
ShinoBee <49558719+ShinObee5@users.noreply.github.com>
2020-05-29 15:46:08 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,5 +1,5 @@
 <?php
-		  if(isset($_POST['SEND'])){
+		
 		    $to = "sonusvareed@gmail.com";
 		    $from = $_REQUEST['email'];
 		    $name = $_REQUEST['name'];
@@ -39,5 +39,6 @@
 
 		    mail($to, $subject, $body, $headers);
 		    header("Location : index.php?mailsend");
-		  }
+	
 
+?>
