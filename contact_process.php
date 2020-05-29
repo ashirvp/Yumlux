fab329700f05a9c28be68364c8b9197d6fd0766a
ShinoBee <49558719+ShinObee5@users.noreply.github.com>
2020-05-29 14:34:41 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,11 +1,43 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "sonusvareed@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
-?>
+<?php
+		  if(isset($_REQUEST['SEND'])){
+		    $to = "sonusvareed@gmail.com";
+		    $from = $_REQUEST['email'];
+		    $name = $_REQUEST['name'];
+		    $subject = $_REQUEST['subject'];
+		    $number = $_REQUEST['number'];
+		    $cmessage = $_REQUEST['message'];
+		
+
+		    $headers = "From: $from";
+			$headers = "From: " . $from . "\r\n";
+			$headers .= "Reply-To: ". $from . "\r\n";
+			$headers .= "MIME-Version: 1.0\r\n";
+			$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+		
+
+		    $subject = "You have a message from your Bitmap Photography.";
+		
+
+		    $logo = 'img/logo.png';
+		    $link = '#';
+		
+
+			$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+			$body .= "<table style='width: 100%;'>";
+			$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+			$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+			$body .= "</td></tr></thead><tbody><tr>";
+			$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+			$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+			$body .= "</tr>";
+			$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+			$body .= "<tr><td></td></tr>";
+			$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+			$body .= "</tbody></table>";
+			$body .= "</body></html>";
+		
+
+		    mail($to, $subject, $body, $headers);
+		    header("Location : index.php?mailsend");
+		  }
 
